680a609b37c4a9f35e95978d0b2b0bea308548db
eriss <erica.garcete@bue.edu.ar>
2017-11-05 17:44:16 -0300

    Cambio de variables Parte1

--- app.py
+++ app.py
@@ -15,21 +15,16 @@ moment = Moment(app)
 
 app.config['SECRET_KEY'] = 'un string que funcione como llave'
 
-#Se trata de abrir los archivos CSV, en caso de que no se encuentrar mostrará un mensaje en la consola. También, se guarda en una lista los usuarios existentes, para que sea más fácil consultar si cuando alguien registre un usuario nuevo, el nombre ya está utilizado o no.
-lista_usuarios = []
+#Creacion de una lista en donde se guadaran los datos de log de usuario.
+#Se encampsulan los posibles errores que puede suceden al no encontrar los archivos csv.
+users_check = []
 try:
-    with open('Usuarios.csv') as archivo:
-        lector = csv.reader(archivo)
-        for linea in lector:
-            lista_usuarios.append(linea[0])
+    with open('usuariosbase.csv') as archivo:
+        leer_csv = csv.reader(archivo)
+        for linea in leer_csv:
+            users_check.append(linea[0])
 except FileNotFoundError:
-    print('Error de CSV de Usuarios')
-
-try:
-    with open('busquedas.csv') as archivo:
-        pass
-except FileNotFoundError:
-    print('No se encuentra el archivo CSV para las busquedas')
+    print('Error de CSV de usuariosbase')
 
 try:
     with open('Ventas.csv') as archivo:
@@ -37,18 +32,12 @@ try:
 except FileNotFoundError:
     print('No se encuentra el archivo CSV de Ventas')
 
+#La pagina esta pensada para que descubra/muestre el menu si la persona se loggea.
 @app.route('/')
 def index():
-#Lee la sesion para ver si el usuario esta logueado, en caso de que no le este le mostrara un mensaje pidiendo loguearse.
     if 'username' in session:
         return render_template('index.html', username=session.get('username'))
-    return render_template('deslogueado.html')
-
-@app.route('/desloguearse', methods=['GET', 'POST'])
-#Se hace un "pop" al diccionario que contiene el usuario, asi cerrando la sesion
-def desloguearse():
-    session.pop('username', None)
-    return redirect('/login')
+    return render_template('sign_off.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -56,7 +45,7 @@ def login():
 #Se abre el archivo que contiene los usuarios y contraseñas, y si las credenciales que ingresa el usuario son encontradas en el archivo, lo loguea.
     if form_logueo.validate_on_submit():
         try:
-            with open('Usuarios.csv') as archivo:
+            with open('usuariosbase.csv') as archivo:
                 #El siguiente try es por si se ingresa un unico campo lo campture en el IndexError
                 try:
                     f = csv.reader(archivo)
@@ -69,9 +58,9 @@ def login():
                             #El return renderiza en index.html con la sesion iniciada (poder ver el menu).
                             return render_template('index.html', username=session.get('username'))
                 except IndexError:
-                    return 'Número de campos en archivo Usuarios es invalido'        
+                    return 'Número de campos en archivo usuariosbase.csv es invalido'        
         except FileNotFoundError:
-            return 'No se encuentra el archivo de Usuarios'
+            return 'No se encuentra el archivo de usuariosbase'
     return render_template('login.html', form=form_logueo, username=session.get('username'))
 
 #abre el archivo csv con la información a mostrar en la tabla, los headers los guarda en una variable, y el resto de la lista en la otra, luego se envía las variables a la plantilla html para que de formato y muestre la tabla.
@@ -86,7 +75,7 @@ def contactos():
                 return render_template('ventas.html', cabeza=primera_linea, cuerpo=lista_ventas, username=session.get('username'))
         except FileNotFoundError:
             return 'No se encuentra el archivo CSV'
-    return render_template('deslogueado.html')
+    return render_template('sign_off.html')
 
 #Busquedas en la tabla (para hacer las busquedas, utilizé el dataFrame de Pandas, que primero lee el archivo y lo guarda en una variable, luego, como argumento le hardcodeo el header de la columna en la cual quiero hacer la busqueda, y paso como dato lo que el usuario haya ingresado para buscar, si lo que se buscó se encuentra en el dataframe, guardara en la variable df2 el resultado. Luego, utilizando el metodo "to_csv" del dataframe, guardo en un archivo el resultado, con formato csv, y hago exactamente lo mismo que en la url anterior, abro el archivo, guardo los headers en una variable, los resultados en otra, y los envio a la plantilla html.
 
@@ -115,9 +104,9 @@ def busqueda_cliente():
                             ventas.append(venta)
                     return render_template('resultado.html', form=form_nombre, cabeza=tupla, cuerpo=ventas, username=session.get('username'))
                 except IndexError:
-                    return 'Número de campos en archivo Usuarios es invalido'           
+                    return 'Numero invalido de datos a corroborar.'           
         return render_template('busqueda_cliente.html', form=form_nombre, df=df, username=session.get('username'))
-    return render_template('deslogueado.html')
+    return render_template('sign_off.html')
 
 @app.route('/busqueda/producto', methods=['GET', 'POST'])
 def busqueda_producto():
@@ -140,14 +129,14 @@ def busqueda_producto():
                         if "CLIENTE" == cliente:
                             tupla = [p[0],p[1],p[2],p[3]]
                         # Este Array guarda las ventas que coincide el cliente
-                        if form_producto.criterio.data == producto:
+                        if form_producto.criterio.data.lower() in producto.lower():
                             venta = [p[0],p[1],p[2],p[3]]
                             ventas.append(venta)
                     return render_template('resultado.html', form=form_producto, cabeza=tupla, cuerpo=ventas, username=session.get('username'))
                 except IndexError:
                     return 'Número de campos en archivo Usuarios es invalido'                           
         return render_template('busqueda_producto.html', form=form_producto, df=df, username=session.get('username'))
-    return render_template('deslogueado.html')
+    return render_template('sign_off.html')
 
 @app.route('/busqueda/cantidad', methods=['GET', 'POST'])
 def busqueda_cantidad():
@@ -175,9 +164,9 @@ def busqueda_cantidad():
                             ventas.append(venta)                            
                     return render_template('resultado.html', form=form_cantidad, cabeza=tupla, cuerpo=ventas, username=session.get('username'))
                 except IndexError:
-                    return 'Número de campos en archivo Usuarios es invalido'                           
+                    return 'Error al encontrar los usuarios y cantidad'                           
         return render_template('busqueda_cantidad.html', form=form_cantidad, df=df, username=session.get('username'))
-    return render_template('deslogueado.html')
+    return render_template('sign_off.html')
 
 
 @app.route('/busqueda/precio', methods=['GET', 'POST'])
@@ -206,9 +195,9 @@ def busqueda_precio():
                             ventas.append(venta)                            
                     return render_template('resultado.html', form=form_precio, cabeza=tupla, cuerpo=ventas, username=session.get('username'))
                 except IndexError:
-                    return 'Número de campos en archivo Usuarios es invalido'                           
+                    return 'Error al buscar el usuario y su precio'                           
         return render_template('busqueda_precio.html', form=form_precio, df=df, username=session.get('username'))
-    return render_template('deslogueado.html')
+    return render_template('sign_off.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -216,17 +205,22 @@ def register():
     if form_registro.validate_on_submit():
         if form_registro.password1.data == form_registro.password2.data:
             try:
-                with open('Usuarios.csv', 'a') as archivo:
+                with open('usuariosbase.csv') as archivo:
                     escritor = csv.writer(archivo)
-                    if form_registro.name.data in lista_usuarios:
+                    if form_registro.name.data in users_check:
                         return "Usuario existente"
                     else:
                         escritor.writerow([form_registro.name.data, form_registro.password1.data])
                         return redirect('login')
             except FileNotFoundError:
-                return 'No se encuentra el archivo CSV de Usuarios'
+                return 'No se encuentra el CSV'
         return "Revise la contraseña"
     return render_template('register.html', form=form_registro)
+
+@app.route('/signoff', methods=['GET', 'POST'])
+def desloguearse():
+    session.pop('username', None)
+    return redirect('/login')
 
 @app.errorhandler(404)
 def no_encontrado(e):
