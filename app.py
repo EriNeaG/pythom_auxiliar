b947885bba1f1c760ac12134573623cfc94da3c6
eriss <erica.garcete@bue.edu.ar>
2017-11-05 11:30:49 -0300

    Creacion del formularios.py para las clases

--- app.py
+++ app.py
@@ -2,11 +2,12 @@
 import csv
 import sys
 import pandas
-from datetime import datetime
+#from datetime import datetime
 from flask import Flask, render_template, request, redirect, url_for, flash, session
-from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, PasswordField
-from wtforms.validators import DataRequired,EqualTo,Regexp
+from formularios import Consulta_Cliente,Consulta_Producto,Consulta_Cantidad,Consulta_Precio, Formulario_Logueo,Formulario_Registro
+#from flask_wtf import FlaskForm
+#from wtforms import StringField, IntegerField, PasswordField
+#from wtforms.validators import DataRequired,EqualTo,Regexp
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_script import Manager
@@ -15,34 +16,6 @@ app = Flask(__name__)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
-
-#Formularios para las consultas al archivo CSV.
-
-class Consulta_Cliente(FlaskForm):
-    criterio = StringField('Escriba el Nombre del Cliente: ', validators=[DataRequired(message="Debe escribir valor")])
-
-class Consulta_Producto(FlaskForm):
-    criterio = StringField('Escriba el Nombre del Producto: ', validators=[DataRequired(message="Debe escribir un valor")])
-
-class Consulta_Cantidad(FlaskForm):
-    criterio = StringField('Escriba la Cantidad que Stock que desea buscar: ', validators=[DataRequired(message="Debe escribir un valor"),Regexp(regex="\d+", message="Solo nùmeros enteros por favor")])
-
-class Consulta_Precio(FlaskForm):
-    criterio = StringField('Escriba el Precio que busca: ', validators=[DataRequired(message="Debe escribir un valor"),Regexp(regex="^(\d|-)?(\d|,)*\.?\d*$", message="Ingrese un precio valido")])
-    #/^[1-9]\d*$/
-
-#Formulario para logueo del usuario.
-
-class Formulario_Logueo(FlaskForm):
-    name = StringField('Usuario:', validators=[DataRequired(message="Debe escribir un nombre de usuario")])
-    password = PasswordField('Contraseña:', validators=[DataRequired(message="Debe escribir una contraseña")])
-
-#Formulario para registro de un nuevo usuario.
-
-class Formulario_Registro(FlaskForm):
-    name = StringField('Usuario:', validators=[DataRequired(message="Debe escribir un nombre de usuario")])
-    password1 = PasswordField('Contraseña:', validators=[DataRequired(message="Debe escribir una contraseña")])
-    password2 = PasswordField('Repita Contraseña:', validators=[DataRequired(message="Debe escribir de nuevo su contraseña"),EqualTo('password1', message='Las contraseñas deben coincidir')])
 
 app.config['SECRET_KEY'] = 'un string que funcione como llave'
 
